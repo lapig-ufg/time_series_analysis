fa02a7439f5d4a33d847eb19fda3748e51efd86c
Oliveira_Santos <claudineisantosnx@gmail.com>
2017-07-17 13:09:31 -0300

    end of field's points extractions

--- src/R/twdtw/Pontos_de_Campo.R
+++ src/R/twdtw/Pontos_de_Campo.R
@@ -5,8 +5,6 @@
 #' shp2 <- shp[-c(2694, 2780, 3242, 3328),]
 #'shapefile(shp2, file ='H:/DATASAN/campo/PontosPastagem_todos/PONTOS_PASTAGEM_ok.shp')
 
-
-
 ###
 ###
 options(scipen = 999)
@@ -14,7 +12,7 @@ options(scipen = 999)
 #Pacotes e funcoes
 library(raster)
 library(forecast)
-library(measurements)
+library(parallel)
 source('../../../src/R/utils/MaxMinFilter.r')
 
 #Arquivos
@@ -34,7 +32,7 @@ r <- brick('E:/pa_br_mod13q1_ndvi_250_2000_2017.tif')
 # names(pontos)[4:395] <- dates
 # write.csv(pontos, file = "H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem.csv", row.names = FALSE)
 
-pontos <- read.csv("H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem_1.csv")
+pontos <- read.csv("H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem.csv")
 
 #Extrair dados de ndvi para os pontos de campo
 CellNumber <- pontos$CellNumber[3001:4000]
@@ -49,10 +47,35 @@ for (i in 1:length(CellNumber)) {
 close(pb) 
 Sys.time() - ST
 
-pontos <- read.csv("H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem_1.csv")
+#pontos <- read.csv("H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem.csv")
 pontos[3001:4000, 4:395] <- pix
 
-write.csv(pontos, file = "H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem_1.csv", row.names = FALSE)
+# write.csv(pontos, file = "H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem.csv", row.names = FALSE)
 View(pontos)
+
+
+###
+###
+#Run na.interp
+ST = Sys.time()
+pontos[,4:395] = t(apply(pontos[,4:395], 1, na.interp))
+Sys.time() - ST
+
+cl <- makeCluster(detectCores() - 1)
+ST = Sys.time()
+pontos[,4:395] = t(parApply(cl = cl, pontos[,4:395], 1, MaxMinFilter, n = 3))
+Sys.time() - ST
+stopCluster(cl)
+
+
+# write.csv(pontos, file = "H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem_maxminfilter.csv", row.names = FALSE)
+# 
+# dim(shp@data)
+# tail(pontos_dt[,1:8])
+# 
+# pontos_dt <- cbind(shp@data[,1:4], pontos)
+# 
+# write.csv(pontos_dt, file = "H:/DATASAN/campo/PontosPastagem_todos/NDVI_PontosPastagem_maxminfilter.csv", row.names = FALSE)
+
 #######################################################################################
 #######################################################################################
